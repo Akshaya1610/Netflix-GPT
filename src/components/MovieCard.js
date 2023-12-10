47b95109099b43d8c8f99d1da96132f8bfd3e7cb
Akshaya <akshayapa16@gmail.com>
2023-12-10 21:14:29 +0530

    video modal css updated

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,20 +1,27 @@
-import React, { useState } from 'react'
-import { IMG_CDN_URL } from '../utils/constants'
-import VideoModal from './VideoModal';
+import React, { useState } from "react";
+import { IMG_CDN_URL } from "../utils/constants";
+import VideoModal from "./VideoModal";
 
-const MovieCard = ({posterPath, movieId}) => {
-  const [openModal, setModalOpen] = useState(false)
-  if(!posterPath) return null;
+const MovieCard = ({ posterPath, movieId }) => {
+  const [openModal, setModalOpen] = useState(false);
+  if (!posterPath) return null;
 
   return (
     <>
-      <div className="w-36 md:w-48 pr-4 movie-card" >
-        <img alt="Movie Card" src={IMG_CDN_URL + posterPath} onClick={()=> setModalOpen(true)}  />
-         {openModal && <VideoModal setOpenModal={setModalOpen} movieId={movieId}/>}
+      <div className="w-36 md:w-48 pr-4 movie-card">
+        <img
+          alt="Movie Card"
+          src={IMG_CDN_URL + posterPath}
+          onClick={() => setModalOpen(true)}
+        />
       </div>
-     
+      <div className="">
+        {openModal && (
+          <VideoModal setOpenModal={setModalOpen} movieId={movieId} />
+        )}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default MovieCard
+export default MovieCard;
--- src/components/VideoModal.js
+++ src/components/VideoModal.js
@@ -1,43 +1,55 @@
-import React, { useEffect, useState } from 'react';
-import { API_OPTIONS } from '../utils/constants'
+import React, { useEffect, useState } from "react";
+import { API_OPTIONS } from "../utils/constants";
 import "./VideoModal.css";
 
-const VideoModal = ({setOpenModal, movieId }) => {
-    const [trailerKey, setTrailerKey] = useState(null)
-    const getCurrentMovieTrailer = async ()=>{
-      const data = await fetch("https://api.themoviedb.org/3/movie/" + movieId +"/videos?language=en-US", API_OPTIONS)
-      const json = await data.json();
-      const filterData = json.results.filter((video) => video.type === "Trailer");
-      const trailer = filterData.length ? filterData[0] : json.results[0];
-      setTrailerKey(trailer)
-    }
+const VideoModal = ({ setOpenModal, movieId }) => {
+  const [trailerKey, setTrailerKey] = useState(null);
+  const getCurrentMovieTrailer = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/" +
+        movieId +
+        "/videos?language=en-US",
+      API_OPTIONS
+    );
+    const json = await data.json();
+    const filterData = json.results.filter((video) => video.type === "Trailer");
+    const trailer = filterData.length ? filterData[0] : json.results[0];
+    setTrailerKey(trailer);
+  };
 
-useEffect(()=>{
+  useEffect(() => {
     getCurrentMovieTrailer();
-},[])
-if(!trailerKey) return
+  }, []);
+  if (!trailerKey) return;
   return (
-  
-   <div id="myModal" className="modal">
-   <div className="modal-content">
+    <div id="myModal" className="modal">
+      <div className="modal-content">
+        <div className="px-2">
+          <span
+            className="close"
+            onClick={() => {
+              setOpenModal(false);
+            }}
+          >
+            &times;
+          </span>
+        </div>
 
-     <div className="px-2">
-       <span className="close"  onClick={() => {
-            setOpenModal(false);
-          }}>&times;</span>
-     </div>
-    
-     <div className="modal-body">
-     <iframe className ='w-full aspect-video'
-      src = {"https://www.youtube.com/embed/"+ trailerKey?.key  + "?autoplay=1"}
-      title = "YouTube video player"
-      allow ="accelerometer; autoplay; clipboard-write;
-       encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
-     </div>
-    
-
-   </div>
- </div>
+        <div className="modal-body">
+          <iframe
+            className="w-full aspect-video"
+            src={
+              "https://www.youtube.com/embed/" +
+              trailerKey?.key +
+              "?autoplay=1&mute=1"
+            }
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write;
+       encrypted-media; gyroscope; picture-in-picture; web-share"
+          ></iframe>
+        </div>
+      </div>
+    </div>
   );
 };
 
